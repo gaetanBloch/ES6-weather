b0865fcbb773b602b810b89aef84e3deb5ebc4e8
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-26 21:45:22 +0200

    Implemented the Get request data from a server with a given url

---
+++ modules/http.js
@@ -0,0 +1,18 @@
+class Http {
+  static fetchData = (url) => {
+    return new Promise(((resolve, reject) => {
+      const http = new XMLHttpRequest();
+      http.open('GET', url);
+      http.onreadystatechange = () => {
+        if (http.readyState === XMLHttpRequest.DONE) {
+          if (http.status === 200) {
+            const responseData = JSON.parse(http.responseText);
+            resolve(responseData);
+          } else {
+            reject('Something went wrong');
+          }
+        }
+      }
+    }))
+  }
+}
