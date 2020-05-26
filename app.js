e74571afcaff5db6276a7150d76b2ca840d44989
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-26 21:37:19 +0200

    Created modules directory and added http module

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-import * as ELEMENTS from './elements.js';
+import * as ELEMENTS from './modules/elements.js';
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', () => {
   const cityName = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
   if (cityName.length === 0) {
     return alert('Please enter a city name');
   }
-  alert(cityName);
 });
