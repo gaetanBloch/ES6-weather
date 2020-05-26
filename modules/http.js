f4e37b889c002c892d413478ff4060c46ce2c6ee
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-26 22:32:34 +0200

    Replace the creation of a promise with async / await

--- modules/http.js
+++ modules/http.js
@@ -1,20 +1,20 @@
 class Http {
-  static fetchData = (url) => {
+  static fetchData = async (url) => {
 
     const errorMessage = 'Something went wrong';
 
-    return new Promise(((resolve, reject) => {
-      fetch(url)
-        .then(response => response.json())
-        .then(responseData => {
-          if(responseData.cod === 200) {
-            resolve(responseData);
-          } else {
-            reject(errorMessage);
-          }
-        })
-        .catch(() => reject(errorMessage));
-    }));
+    let responseData;
+    try {
+      const response = await fetch(url);
+      responseData = await response.json();
+    } catch (error) {
+      throw new Error(errorMessage);
+    }
+    if (responseData.cod === 200) {
+      return responseData;
+    } else {
+      throw new Error(errorMessage);
+    }
   };
 }
 
