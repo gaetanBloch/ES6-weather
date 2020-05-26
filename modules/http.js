339751f9cdc95845e6932501783a8961ac915142
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-26 22:34:14 +0200

    Refactor http fetchData method

--- modules/http.js
+++ modules/http.js
@@ -2,18 +2,21 @@ class Http {
   static fetchData = async (url) => {
 
     const errorMessage = 'Something went wrong';
+    const throwError = () => {
+      throw new Error(errorMessage);
+    };
 
     let responseData;
     try {
       const response = await fetch(url);
       responseData = await response.json();
     } catch (error) {
-      throw new Error(errorMessage);
+      throwError();
     }
     if (responseData.cod === 200) {
       return responseData;
     } else {
-      throw new Error(errorMessage);
+      throwError();
     }
   };
 }
